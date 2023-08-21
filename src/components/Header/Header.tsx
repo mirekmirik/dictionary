a866c83c44c6b4614b1953a2c944fcdbc4830da5
MiroslavGit <benya.senya.16@gmail.com>
2023-08-21 21:17:06 +0300

    feat:second commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Fonts } from './Fonts'
+import { Theme } from './Theme'
+
+const Header = () => {
+    return (
+        <>
+            <div>Header</div>
+            <Fonts />
+            <Theme />
+        </>
+    )
+}
+
+export default Header
