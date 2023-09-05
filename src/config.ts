164e1097b9cd421a116d3ef46bacd72f9af00ba8
Miroslav Bondik <benya.senya.16@gmail.com>
2023-09-05 19:55:23 +0300

    Add files via upload
    feat(backend-link): change link on backend

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const API_FONTS = `https://www.googleapis.com/webfonts/v1/webfonts?key=${process.env.REACT_APP_KEY_FONTS}`
 export const API_DICTIONARY = `https://api.dictionaryapi.dev/api/v2/entries/en`
-export const SERVER = `http://localhost:5000`
+export const SERVER = `https://dictionary-backend-layj.onrender.com`
 export const ADD_MARKED_SERVER = 'api/dictionary/addMarked'
 export const GET_MARKED_SERVER = 'api/dictionary/getMarked'
 export const ADD_RECENTS_SERVER = 'api/dictionary/addRecents'
